742fbb2c8840b9577cfb3073d74022670347a58b
hmohamed7 <hannah.mohamed@fdmgroup.com>
2015-12-04 15:15:44 +0000

    Added Add

---
+++ Model/Add/AddMission.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Add
+{
+    class AddMission
+    {
+        RebellionDataEntities context;
+
+        public AddMission(RebellionDataEntities Context)
+        {
+            context = Context;
+        }
+
+        public void CreateNewMission(string MissionName, int MissionID, string Type, int GroupID)
+        {
+            string missionName = MissionName;
+            int missionId = MissionID;
+            string type = Type;
+            int groupId = GroupID;
+            using (context)
+            {
+                Mission mission = new Mission();
+                mission.mission_name = missionName;
+                mission.mission_id = missionId;
+                mission.type = type;
+                mission.Fk_group_id = groupId;
+                context.Missions.Add(mission);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Model/Add/AddRebel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Add
+{
+    class AddRebel
+    {
+        RebellionDataEntities context;
+
+        public AddRebel(RebellionDataEntities Context)
+        {
+            context = Context;
+        }
+
+        public void createNewRebel(string Codename, int RebelID, int GroupID)
+        {
+            string codename = Codename;
+            int rebelId = RebelID;
+            int groupId = GroupID;
+            using (context)
+            {
+                Rebel rebel = new Rebel();
+                rebel.code_name = codename;
+                rebel.rebel_id = rebelId;
+                rebel.Fk_group_id = groupId;
+                context.Rebels.Add(rebel);
+                context.SaveChanges();
+            }
+        }
+        
+    }
+}
