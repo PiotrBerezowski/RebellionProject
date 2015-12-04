4265215eceea510eea5b04e85619a3968d5b5b6f
Francelp <francel_piedra@hotmail.com>
2015-12-04 15:51:43 +0000

    ViewModel Update

--- GUI/Navigation/AddPageViewModel.cs
+++ GUI/Navigation/AddPageViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace GUI.Navigation
 {
-    public class AddPageViewModel
+    public class AddPageViewModel : INotifyPropertyChanged
     {
         private string _source;
         public string source
@@ -18,6 +20,44 @@ namespace GUI.Navigation
         public AddPageViewModel()
         {
             source = "AddPage.xaml";
+        }
+
+        private int _rebelId;
+        public int rebelId
+        {
+            get { return _rebelId; }
+            set
+            {
+                _rebelId = value;
+                onPropertyChanged("rebelId");
+            }
+        }
+
+        private string _name;
+        public string name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+
+            }
+        }
+
+        private int _territoryId;
+        public int territoryId
+        {
+            get { return _territoryId; }
+            set { _territoryId = value; onPropertyChanged("territoryId"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void onPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
